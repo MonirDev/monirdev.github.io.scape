29a3b4d123f89dad0ed3d63d5b8afd70124aace5
Monir Hossain <mdmonir1hossain@gmail.com>
2024-07-09 20:56:01 +0600

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1625943117"
+    serviceWorkerVersion: "429161605"
   }
 });
